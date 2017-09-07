ca6196cf42f04f604cfe89d942dda2155d750a57
SoraMane <31667047+SoraMane@users.noreply.github.com>
2017-09-07 16:37:39 +0530

    Made edit to two files

--- app/src/main/java/aacci/sourabh/myapplication/MainActivity.java
+++ app/src/main/java/aacci/sourabh/myapplication/MainActivity.java
@@ -7,7 +7,11 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        int i = 0;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        for(;i<10;i++){
+            i++;
+        }
     }
 }
